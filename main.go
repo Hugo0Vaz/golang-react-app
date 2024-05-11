2b31189c1cb340b5f1be9174f48cb5eca6fe617b
Hugo Vaz <hugomartinsvaz@gmail.com>
2024-05-10 23:17:36 -0300

    criando executável de go para rodar o aplicativo react

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
 )
 
+var (
+	//go:embed react/my-app/build
+	web embed.FS
+)
+
+const defaultPort = "8080"
+
 func main() {
-	fmt.Println("Hello, World!")
+	dist, _ := fs.Sub(web, "react/my-app/build")
+
+	http.Handle(
+		"/", http.FileServer(http.FS(dist)),
+	)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = defaultPort
+	}
+
+	fmt.Printf("O aplicativo está disponível em http://localhost:%s", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
